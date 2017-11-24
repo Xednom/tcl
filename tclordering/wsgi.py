a487fa874e18d0cea26a4489d04cabbb58381aea
xednom <monde.lacanlalay@gmail.com>
2017-11-25 05:32:32 +0800

    Fixed the wsgi.py

--- tclordering/wsgi.py
+++ tclordering/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for tclordering project.
+WSGI config for systems project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tclordering.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
