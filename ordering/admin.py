aa65c117bb2ac88b2b38ce57eb0bcd2dd9478d02
Richmond <monde.lacanlalay@gmail.com>
2017-12-02 08:57:46 +0800

    Added admin.py for admin

--- ordering/admin.py
+++ ordering/admin.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from . models import Order, Product, Inventory
 
-# Register your models here.
+
+class OrderProfile(admin.ModelAdmin):
+    list_display = ('shipment_provider', 'ordered_by', 'last_name', 'first_name', 'middle_name', 'address', 'barangay', 'city_and_municipality', 'province', 'phone', 'quantity', 'order', 'status', 'special_instructions')
+
+
+class InventoryProfile(admin.ModelAdmin):
+    list_display = ('product', 'stock_in', 'stock_out', 'balance', 'particulars')
+
+
+#fields = ('image_tag',)
+#readonly_fields = ('image_tag',)
+
+admin.site.register(Order, OrderProfile)
+admin.site.register(Inventory, InventoryProfile)
+admin.site.register(Product)
