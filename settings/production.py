7968f99637b85982b165c41e64548b0cde2cb9d4
Richmond <monde.lacanlalay@gmail.com>
2017-12-09 17:44:16 +0800

    Update; removed a comment in gitignore and changed static root in prod

--- settings/production.py
+++ settings/production.py
@@ -183,7 +183,7 @@ USE_TZ = True
 
 
 # ######### MEDIA CONFIGURATION
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'theme/img')
